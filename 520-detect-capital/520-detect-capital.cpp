98b8b8d02c9054f11617425de1e7879d5d8bb21b
Vishnu Rudhva <vishnurudhva01@gmail.com>
2022-01-15 15:27:49 +0530

    Time: 3 ms (37.70%), Space: 6.1 MB (18.42%) - LeetHub

---
+++ 520-detect-capital/520-detect-capital.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool detectCapitalUse(string word) {
+        if(isupper(word[0]) && isupper(word[1]))
+        {
+            for(int i = 0; i < word.length(); i++)
+                if(!isupper(word[i])) return false;
+        } else {
+            for(int i = 1; i < word.length(); i++)
+                if(!islower(word[i])) return false;
+        }
+        
+        return true;
+    }
+};
