16f321657eed9122eabe3917e428676904c0d075
Vishnu Rudhva <vishnurudhva01@gmail.com>
2022-01-15 16:00:43 +0530

    Time: 843 ms (42.82%), Space: 255.4 MB (19.59%) - LeetHub

---
+++ 2080-range-frequency-queries/2080-range-frequency-queries.cpp
@@ -0,0 +1,22 @@
+class RangeFreqQuery {
+public:
+    vector<int> ids[10001] = {};
+
+    RangeFreqQuery(vector<int>& arr) {
+        for (int i = 0; i < arr.size(); ++i)
+            ids[arr[i]].push_back(i);
+    }
+    
+    int query(int left, int right, int v) {
+        return upper_bound(begin(ids[v]), end(ids[v]), right) - 
+        lower_bound(begin(ids[v]), end(ids[v]), left);
+    }
+    
+    
+};
+
+/*
+ * Your RangeFreqQuery object will be instantiated and called as such:
+ * RangeFreqQuery* obj = new RangeFreqQuery(arr);
+ * int param_1 = obj->query(left,right,value)
+ */
