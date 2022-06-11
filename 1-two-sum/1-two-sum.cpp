17be05df98e484b70240fc7cf615c417162fefdb
Vishnu Rudhva <vishnurudhva01@gmail.com>
2022-06-11 12:03:37 +0530

    Time: 19 ms (60.17%), Space: 10.7 MB (52.99%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -4,11 +4,10 @@ public:
         unordered_map<int, int> m;
         
         for(int i = 0; i < nums.size(); i++) {
-            if (m.find(target - nums[i]) == m.end())
-                m[target - nums[i]] = i;
-            if (m.find(nums[i]) != m.end() && m[nums[i]] != i) {
+            if (m.find(nums[i]) != m.end())
                 return {i, m[nums[i]]};
-            }
+            m[target - nums[i]] = i;
+            
         }
         
         
