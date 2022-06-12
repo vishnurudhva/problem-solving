42ff5ed9fd2b66814650bfe6c05c6586b32af056
Vishnu Rudhva <vishnurudhva01@gmail.com>
2022-06-12 07:17:56 +0530

    Time: 15 ms (78.31%), Space: 8.9 MB (14.95%) - LeetHub

--- 383-ransom-note/383-ransom-note.cpp
+++ 383-ransom-note/383-ransom-note.cpp
@@ -1,20 +1,17 @@
 class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
-        unordered_map<char, int> map;
-        int total_count = ransomNote.length();
+        unordered_map<char, int> m;
+        
+        for(char c: magazine)
+            m[c]++;
+        
         for(char c: ransomNote) {
-            map[c]++;
+            if (m.find(c) == m.end() || m[c] == 0)
+                return false;
+            m[c]--;
         }
         
-        for(char c: magazine) {
-            if(map.find(c) != map.end() && map[c] != 0) {
-                map[c]--;
-                total_count--;
-            }
-        }
-        
-        
-        return total_count == 0;
+        return true;
     }
 };
