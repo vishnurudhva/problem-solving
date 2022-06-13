1b09dadb645c9159145292c5f6114f1bdb9b54a0
Vishnu Rudhva <vishnurudhva01@gmail.com>
2022-06-13 20:23:47 +0530

    Time: 2 ms (95.63%), Space: 7.2 MB (98.28%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.cpp
+++ 125-valid-palindrome/125-valid-palindrome.cpp
@@ -2,13 +2,14 @@ class Solution {
 public:
     bool isPalindrome(string s) {
         int i = 0, j = s.length() - 1;
-        while(i < j) {
-            while(i < s.length() && !iswalnum(s[i])) i++;
-            while(j >= 0 && !iswalnum(s[j])) j--;
-            if(i > j || i >= s.length() || j < 0) return true;
-            if(tolower(s[i]) != tolower(s[j])) return false;
-            i++;
-            j--;
+        
+        while (i < j) {
+            while (i < s.length() && !isalnum(s[i])) i++;
+            while (j >= 0 && !isalnum(s[j])) j--;
+            
+            if(i > j) break;
+            
+            if(tolower(s[i++]) != tolower(s[j--])) return false;
         }
         
         return true;
