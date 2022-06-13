e5489a5519a701ce936a84268fb1458a456504eb
Vishnu Rudhva <vishnurudhva01@gmail.com>
2022-06-13 21:57:20 +0530

    Time: 6 ms (94.27%), Space: 14.2 MB (37.65%) - LeetHub

--- 733-flood-fill/733-flood-fill.cpp
+++ 733-flood-fill/733-flood-fill.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor) {
-        if(image[sr][sc] == newColor) return image;
-        fill(image, sr, sc, newColor, image[sr][sc]);
+        if (newColor == image[sr][sc]) return image;
+        int oc = image[sr][sc];
+        queue<pair<int, int>> q;
+        q.push({sr, sc});
+        
+        while (!q.empty()) {
+            int i = q.front().first, j = q.front().second;
+            q.pop();
+            image[i][j] = newColor;
+            if (i - 1 >= 0 && image[i - 1][j] == oc) q.push({i - 1, j});
+            if (i + 1 < image.size() && image[i + 1][j] == oc) q.push({i + 1, j});
+            if (j - 1 >= 0 && image[i][j - 1] == oc) q.push({i, j - 1});
+            if (j + 1 < image[0].size() && image[i][j + 1] == oc) q.push({i, j + 1});
+        }
         
         return image;
-    }
-    
-    void fill(vector<vector<int>>& image, int sr, int sc, int newColor, int currentColor) {
-        if(image[sr][sc] != currentColor) return;
-        
-        image[sr][sc] = newColor;
-        
-        if(sr + 1 < image.size()) fill(image, sr + 1, sc, newColor, currentColor);
-        if(sr - 1 >= 0) fill(image, sr - 1, sc, newColor, currentColor);
-        if(sc + 1 < image[0].size()) fill(image, sr, sc + 1, newColor, currentColor);
-        if(sc - 1 >= 0) fill(image, sr, sc - 1, newColor, currentColor);
     }
 };
