4760eb0d004a11d27f6484c942cb84e1e16dbd9e
Vishnu Rudhva <vishnurudhva01@gmail.com>
2022-06-13 20:24:43 +0530

    Time: 11 ms (29.66%), Space: 7.4 MB (75.22%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.cpp
+++ 125-valid-palindrome/125-valid-palindrome.cpp
@@ -4,10 +4,9 @@ public:
         int i = 0, j = s.length() - 1;
         
         while (i < j) {
-            while (i < s.length() && !isalnum(s[i])) i++;
-            while (j >= 0 && !isalnum(s[j])) j--;
+            while (i < j && !isalnum(s[i])) i++;
+            while (i < j && !isalnum(s[j])) j--;
             
-            if(i > j) break;
             
             if(tolower(s[i++]) != tolower(s[j--])) return false;
         }
