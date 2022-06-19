e82dfe3f95c8c23e5db739e6b26920923418d63f
Vishnu Rudhva <vishnurudhva01@gmail.com>
2022-06-19 21:35:02 +0530

    Time: 6 ms (36.82%), Space: 6.8 MB (30.56%) - LeetHub

--- 67-add-binary/67-add-binary.cpp
+++ 67-add-binary/67-add-binary.cpp
@@ -3,18 +3,17 @@ public:
     string addBinary(string a, string b) {
         string result = "";
         
-        int i = a.length() - 1, j = b.length() - 1;
-        int carry = 0;
-        while (i >= 0 || j >= 0) {
-            int x = i >= 0 ? a[i] - '0': 0;
-            int y = j >= 0 ? b[j] - '0': 0;
+        int i = a.length() - 1, j = b.length() - 1, s = 0;
+        while (i >= 0 || j >= 0 || s == 1) {
+            s += i >= 0 ? a[i] - '0': 0;
+            s += j >= 0 ? b[j] - '0': 0;
             
-            result = to_string((x + y + carry) % 2) + result;
-            carry = (x + y + carry) / 2;
+            result = to_string(s % 2) + result;
+            s = s / 2;
             i--;
             j--;
         }
         
-        return carry ? "1" + result: result;
+        return result;
     }
 };
