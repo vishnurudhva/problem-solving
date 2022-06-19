3381d0df1da5f6369d3469f8dbb42ddfc9a945c0
Vishnu Rudhva <vishnurudhva01@gmail.com>
2022-06-19 18:32:40 +0530

    Time: 3 ms (36.66%), Space: 5.7 MB (99.19%) - LeetHub

--- 278-first-bad-version/278-first-bad-version.cpp
+++ 278-first-bad-version/278-first-bad-version.cpp
@@ -4,17 +4,14 @@
 class Solution {
 public:
     int firstBadVersion(int n) {
-        long l = 0, r = n, m;
-        while(l <= r)
-        {
-            m = (l + r) / 2;
-            if(isBadVersion(m) == false && isBadVersion(m + 1) == true)
-                return m + 1;
-            else if(isBadVersion(m) == true)
-                r = m - 1;
-            else
-                l = m + 1;
+        int i = 1, j = n;
+        while(i <= j) {
+            int mid = i + (j - i) / 2;
+            if (isBadVersion(mid) && !isBadVersion(mid - 1)) return mid;
+            if (isBadVersion(mid)) j = mid - 1;
+            else i = mid + 1;
         }
-        return m + 1;
+        
+        return -1;
     }
 };
