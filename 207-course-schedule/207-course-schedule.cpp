1021f36e658c581e2a840bb611ef397a324ae42a
Vishnu Rudhva <vishnurudhva01@gmail.com>
2022-06-24 22:21:58 +0530

    Time: 69 ms (6.90%), Space: 16.1 MB (9.35%) - LeetHub

--- 207-course-schedule/207-course-schedule.cpp
+++ 207-course-schedule/207-course-schedule.cpp
@@ -1,26 +1,35 @@
 class Solution {
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<vector<int>> course_dependency(numCourses);
-        vector<int> degree(numCourses, 0), bfs;
-        for(vector<int> course_pair: prerequisites)
-        {
-            course_dependency[course_pair[1]].push_back(course_pair[0]);
-            degree[course_pair[0]]++;
+        unordered_set<int> visited;
+        unordered_set<int> done;
+        unordered_map<int, vector<int>> adjList; 
+        
+        for (vector<int> pre: prerequisites) {
+            adjList[pre[0]].push_back(pre[1]);
+            
+            if (adjList.find(pre[1]) == adjList.end())
+                adjList[pre[1]] = {};
         }
         
-        for(int i = 0; i < numCourses; i++)
-        {
-            if(degree[i] == 0)
-                bfs.push_back(i);
+        for (int i = 0; i < numCourses; i++) {
+            if(done.find(i) == done.end() && !canFinishHelper(adjList, i, visited, done))
+                return false;
         }
         
-        for(int i = 0; i < bfs.size(); i++)
-            for(int course: course_dependency[bfs[i]])
-                if(--degree[course] == 0)
-                    bfs.push_back(course);
-        
-        return bfs.size() == numCourses;
+        return true;
     }
     
+    bool canFinishHelper(unordered_map<int, vector<int>>& adjList, int currentCourse, unordered_set<int>& visited, unordered_set<int>& done) {
+        if (visited.find(currentCourse) != visited.end()) return false;
+        if (done.find(currentCourse) != done.end()) return true;
+        visited.insert(currentCourse);
+        for (int i = 0; i < adjList[currentCourse].size(); i++) {
+            if (!canFinishHelper(adjList, adjList[currentCourse][i], visited, done))
+                return false;
+        }
+        visited.erase(currentCourse);
+        done.insert(currentCourse);
+        return true;
+    }
 };
