cb4b2226457f730ecfb3cde347237ca688884964
Vishnu Rudhva <vishnurudhva01@gmail.com>
2022-06-27 08:31:48 +0530

    Time: 45 ms (67.79%), Space: 12.3 MB (84.17%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,27 +1,26 @@
 class Solution {
 public:
     int numIslands(vector<vector<char>>& grid) {
-        int numberOfIslands = 0;
-        for(int i = 0; i < grid.size(); i++)
-        {
-            for(int j = 0; j < grid[i].size(); j++)
-            {
-                if(grid[i][j] == '1'){
-                    flood(grid, i, j);
-                    numberOfIslands++;
+        int count = 0;
+        
+        for (int i = 0; i < grid.size(); i++) {
+            for (int j = 0; j < grid[i].size(); j++) {
+                if (grid[i][j] == '1') {
+                    count++;
+                    dissolveIsland(grid, i, j);
                 }
             }
         }
-        return numberOfIslands;
+        
+        return count;
     }
     
-    void flood(vector<vector<char>>& grid, int i, int j) {
-        if(i < 0 || j < 0 || i >= grid.size() || j >= grid[0].size() || grid[i][j] == '0')
-            return;
+    void dissolveIsland(vector<vector<char>>& grid, int i, int j) {
+        if (i < 0 || i >= grid.size() || j < 0 || j >= grid[0].size() || grid[i][j] == '0') return;
         grid[i][j] = '0';
-        flood(grid, i - 1, j);
-        flood(grid, i + 1, j);
-        flood(grid, i, j - 1);
-        flood(grid, i, j + 1);
+        dissolveIsland(grid, i + 1, j);
+        dissolveIsland(grid, i - 1, j);
+        dissolveIsland(grid, i, j + 1);
+        dissolveIsland(grid, i, j - 1);        
     }
 };
