89bb2b67e213a1512a949b8fc729bf6eb569076f
Vishnu Rudhva <vishnurudhva01@gmail.com>
2022-06-28 06:24:54 +0530

    Time: 11 ms (58.76%), Space: 13.1 MB (79.09%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.cpp
+++ 994-rotting-oranges/994-rotting-oranges.cpp
@@ -1,38 +1,38 @@
 class Solution {
 public:
     
-    void downRight(vector<vector<int>>& grid, int i, int j, int rottenBy) {
+    void canBeRotten(vector<vector<int>>& grid, int i, int j, queue<pair<int, int>>& q, int& freshCount) {
         if (i < 0 || j < 0 || i >= grid.size() || j >= grid[i].size() || grid[i][j] == 0 || grid[i][j] == 2)
             return;
-        if (grid[i][j] < 0)
-            if (rottenBy <= grid[i][j]) return;
-        grid[i][j] = rottenBy;
-        downRight(grid, i + 1, j, rottenBy - 1);
-        downRight(grid, i, j + 1, rottenBy - 1);
-        downRight(grid, i - 1, j, rottenBy - 1);
-        downRight(grid, i, j - 1, rottenBy - 1);
+        freshCount--;
+        q.push({i, j});
+        grid[i][j] = 2;
     }
     
     int orangesRotting(vector<vector<int>>& grid) {
+        queue<pair<int, int>> q;
+        int freshCount = 0;
         for (int i = 0; i < grid.size(); i++) {
             for (int j = 0; j < grid[i].size(); j++) {
                 if (grid[i][j] == 2) {
-                    downRight(grid, i + 1, j, -1);
-                    downRight(grid, i, j + 1, -1);
-                    downRight(grid, i - 1, j, -1);
-                    downRight(grid, i, j - 1, -1);
-                }
+                    q.push({i, j});
+                } else if (grid[i][j] == 1) freshCount++;
             }
         }
-        int result = INT_MAX;
-        for (int i = 0; i < grid.size(); i++) {
-            for (int j = 0; j < grid[i].size(); j++) {
-                if (grid[i][j] == 1) return -1;
-                if (grid[i][j] < 0)
-                    result = min(result, grid[i][j]);
+        int result = -1;
+        while (!q.empty()) {
+            int size = q.size();
+            for (int i = 0; i < size; i++) {
+                pair<int, int> p = q.front(); q.pop();
+                int r = p.first, c = p.second;
+                canBeRotten(grid, r - 1, c, q, freshCount);
+                canBeRotten(grid, r + 1, c, q, freshCount);
+                canBeRotten(grid, r, c - 1, q, freshCount);
+                canBeRotten(grid, r, c + 1, q, freshCount);
             }
+            result++;
         }
         
-        return result == INT_MAX ? 0 : -1 * result;
+        return freshCount ? -1 : result == -1 ? 0 : result;
     }
 };
