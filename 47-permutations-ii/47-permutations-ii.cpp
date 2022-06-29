07af18a6af6983b54a81aa96ef3f929473f39df4
Vishnu Rudhva <vishnurudhva01@gmail.com>
2022-06-29 08:15:20 +0530

    Time: 16 ms (47.69%), Space: 10.3 MB (29.33%) - LeetHub

--- 47-permutations-ii/47-permutations-ii.cpp
+++ 47-permutations-ii/47-permutations-ii.cpp
@@ -1,30 +1,28 @@
 class Solution {
 public:
     vector<vector<int>> permuteUnique(vector<int>& nums) {
-        unordered_map<int, int> map;
-        for(int i: nums)
-            map[i]++;
         vector<vector<int>> result;
-        generate(map, {}, nums.size(), result);
+        unordered_map<int, bool> map;
+        sort(nums.begin(), nums.end());
+        for (int num: nums)
+            map[num] = false;
+        generate(result, {}, nums, map);
         return result;
     }
     
-    void generate(unordered_map<int, int>& map, vector<int> curr, int size, vector<vector<int>>& result)
-    {
-        if(curr.size() == size) {
-            result.push_back(curr);
+    void generate(vector<vector<int>>& result, vector<int> temp, vector<int>& nums, unordered_map<int, bool>& map) {
+        if (temp.size() == nums.size()) {
+            result.push_back(temp);
             return;
         }
         
-        for(auto num: map)
-        {
-            if(num.second == 0) continue;
-            
-            curr.push_back(num.first);
-            map[num.first]--;
-            generate(map, curr, size, result);
-            map[num.first]++;
-            curr.pop_back();
+        for (int i = 0; i < nums.size(); i++) {
+            if (map[i] || i > 0 && nums[i] == nums[i - 1] && !map[i - 1]) continue;
+            map[i] = true;
+            temp.push_back(nums[i]);
+            generate(result, temp, nums, map);
+            temp.pop_back();
+            map[i] = false;
         }
     }
 };
