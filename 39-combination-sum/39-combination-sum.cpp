dc1bced968c3e67790fa80a3405c55653a414aa3
Vishnu Rudhva <vishnurudhva01@gmail.com>
2022-06-29 09:48:08 +0530

    Time: 440 ms (5.02%), Space: 85.3 MB (9.92%) - LeetHub

--- 39-combination-sum/39-combination-sum.cpp
+++ 39-combination-sum/39-combination-sum.cpp
@@ -2,25 +2,19 @@ class Solution {
 public:
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         vector<vector<int>> result;
-        generate(candidates, {}, 0, target, result);
+        generate(candidates, target, result, 0, {}, 0);
         return result;
     }
     
-    void generate(vector<int>& nums, vector<int> curr, int k, int target, vector<vector<int>>& result) {
-        if(0 == target) 
-        {
-            result.push_back(curr);
+    void generate(vector<int>& candidates, int& target, vector<vector<int>>& result, int currentSum, vector<int> temp, int i) {
+        if (currentSum == target) {
+            result.push_back(temp);
             return;
         }
-        
-        if(target < 0)
-            return;
-        
-        for(int i = k; i < nums.size(); i++) {
-            curr.push_back(nums[i]);
-            generate(nums, curr, i, target - nums[i], result);
-            curr.pop_back();
-        }
-    
+        if (i >= candidates.size() || currentSum > target) return;
+        generate(candidates, target, result, currentSum, temp, i + 1);
+        temp.push_back(candidates[i]);
+        generate(candidates, target, result, currentSum + candidates[i], temp, i);
+        temp.pop_back();
     }
 };
