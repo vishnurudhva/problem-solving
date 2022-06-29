84cff0ec5063be5d7492f42d346c1869461b4854
Vishnu Rudhva <vishnurudhva01@gmail.com>
2022-06-29 10:02:27 +0530

    Time: 115 ms (5.51%), Space: 16.8 MB (10.87%) - LeetHub

--- 40-combination-sum-ii/40-combination-sum-ii.cpp
+++ 40-combination-sum-ii/40-combination-sum-ii.cpp
@@ -2,27 +2,28 @@ class Solution {
 public:
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
         vector<vector<int>> result;
+        unordered_map<int, bool> map;
+        for (int can: candidates)
+            map[can] = false;
         sort(candidates.begin(), candidates.end());
-        generate(candidates, 0, {}, target, result);
+        generate(candidates, target, result, {}, 0, map);
         return result;
     }
     
-    void generate(vector<int>& nums, int k, vector<int> curr, int target, vector<vector<int>>& result)
-    {
-        if(target == 0) {
-            result.push_back(curr);
-        }
-        
-        if(target < 0)
+    void generate(vector<int>& candidates, int target, vector<vector<int>>& result, vector<int> temp, int i, unordered_map<int, bool>& map) {
+        if (0 == target) {
+            result.push_back(temp);
             return;
+        }
+        if (i >= candidates.size() || target < 0) return;
         
-        for(int i = k; i < nums.size(); i++)
-        {
-            if(i > k && nums[i] == nums[i - 1]) continue;
-            if(nums[i] > target) break;
-            curr.push_back(nums[i]);
-            generate(nums, i + 1, curr, target - nums[i], result);
-            curr.pop_back();
+        for (int j = i; j < candidates.size(); j++) {
+            if (map[j] || j > 0 && candidates[j] == candidates[j - 1] && !map[j - 1]) continue;
+            map[j] = true;
+            temp.push_back(candidates[j]);
+            generate(candidates, target - candidates[j], result, temp, j + 1, map);
+            temp.pop_back();
+            map[j] = false;
         }
     }
 };
