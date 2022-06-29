b42091b04258a76a67f38526119a07ff78a2fb4f
Vishnu Rudhva <vishnurudhva01@gmail.com>
2022-06-29 08:06:46 +0530

    Time: 38 ms (5.04%), Space: 11.6 MB (5.13%) - LeetHub

--- 46-permutations/46-permutations.cpp
+++ 46-permutations/46-permutations.cpp
@@ -2,21 +2,26 @@ class Solution {
 public:
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>> result;
-        generate(result, {}, nums);
+        unordered_map<int, bool> map;
+        for (int num: nums)
+            map[num] = false;
+        generate(result, {}, nums, map);
         return result;
     }
     
-    void generate(vector<vector<int>>& result, vector<int> temp, vector<int>& nums) {
+    void generate(vector<vector<int>>& result, vector<int> temp, vector<int>& nums, unordered_map<int, bool> map) {
         if (temp.size() == nums.size()) {
             result.push_back(temp);
             return;
         }
         
         for (int i = 0; i < nums.size(); i++) {
-            if (find(temp.begin(), temp.end(), nums[i]) != temp.end()) continue;
+            if (map[i]) continue;
+            map[i] = true;
             temp.push_back(nums[i]);
-            generate(result, temp, nums);
+            generate(result, temp, nums, map);
             temp.pop_back();
+            map[i] = false;
         }
     }
 };
